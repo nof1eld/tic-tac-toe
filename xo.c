586e4ff5d3b9b7db4dee35d3beecead19e367e35
nof1eld <imadeddine799@gmail.com>
2024-03-17 13:57:00 +0100

    test-ready

--- xo.c
+++ xo.c
@@ -5,6 +5,7 @@ char board[3][3];
 char column = '1';
 char X_column, O_column;
 
+//fill the board with numbers from 1 to 9
 void setBoard(){
     for (int i = 0; i < 3; i++)
     {
@@ -15,23 +16,35 @@ void setBoard(){
         }    
     }
 }
-
+//draw the board for the user to pick a column (numerated from 1 to 9)
 void drawBoard(){
     for (int i = 0; i < 3; i++)
     {
-       for (int j = 0; j < 3; j++)
-       {
-           printf("%c",board[i][j]);
-           if(j<2){
-               printf(" | ");
-           }   
-       }
-       printf("\n");
+        if (i==0)
+        {
+           printf("\n");
+        }
+        
+        
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%c",board[i][j]);
+            if(j<2){
+                printf(" | ");
+            }   
+        }
+        if(i<2){
+               printf("\n----------\n");
+            } else {
+                printf("\n");
+            }
+             
+       
     }
 }
-
+//accept input(column number) from player X
 void fillBoard_X(){
-    printf("Enter column number: ");
+    printf("\n(Player X) Enter column number: ");
     scanf(" %c", &X_column);
     for (int i = 0; i < 3; i++)
     {
@@ -43,10 +56,12 @@ void fillBoard_X(){
             }
         }    
     }
+    //draw the board for next turn
     drawBoard();
 }
+//same here (player O)
 void fillBoard_O(){
-    printf("Enter column number: ");
+    printf("\n(Player O) Enter column number: ");
     scanf(" %c", &O_column);
     for (int i = 0; i < 3; i++)
     {
@@ -60,76 +75,81 @@ void fillBoard_O(){
     }
     drawBoard();
 }
-
-int checkWinner(void){
-    return 0;
+//check for any win case
+int checkWin(void){
+    //check x axis
     for (int j = 0; j < 3; j++)
     {
-        if (board[0][j] == board[1][j])
+        if (board[j][1] == board[j][0] && board[j][0] == board[j][2])
         {
             return 1;
             break;
-        } else
-        {
-            return 0;
         }
     }
-    for (int i = 0; i < 3 && checkWinner()!= 1; i++)
+    //check y axis
+    for (int i = 0; i < 3; i++)
     {
-        if (board[i][0] == board[i][1] && board[i][0] == board[i][2])
+        if (board[0][i] == board[1][i] && board[0][i] == board[2][i])
         {
             return 1;
             break;
-        } else
-        {
-            return 0;
         }
         
     }
-    for (int i = 0; i < 3 && checkWinner()!= 1; i++)
+    //check diagonals
+     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-           if ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) || (board[2][0] == board[1][1] && board[1][1] == board[0][2]))
+           if ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) 
+           || (board[2][0] == board[1][1] && board[1][1] == board[0][2]))
         {
             return 1;
             break;
-        } else
-        {
-            return 0;
-        }
+        } 
         }
         
     }
-    
+
 }
-void checkwinner(){
+//decide if there is a win or tie and decide the winner (according to previous checkWin()), then either stops or continues the game
+void checkWinner(){
+    //count number of turns played (must not exceed 9 ==> 5 for X, 4 for O)
+    int turn = 0;
     for (int i = 0; i < 5; i++)
     {
-        printf("checkwinner value = %d\n", checkWinner());
-        if (checkWinner() != 1)
+        if (checkWin() != 1)
         {
             fillBoard_X();
+            turn++;
         } else
         {
-            printf("The winner is: O\n");
+            //if previous turn of player O made him win
+            printf("\nPlayer O has won !\n");
             break;
         }
-        printf("checkwinner value = %d\n", checkWinner());
-        if (checkWinner() != 1)
+        if (checkWin() != 1 && turn != 9)
         {
             fillBoard_O();
-        } else
+            turn++;
+        } else if (turn != 9) // && checkWin() == 1
         {
-            printf("The winner is: X\n");
+            printf("\nPlayer X has won !");
+            break;
+        } 
+        else //9 turns were played with no winner
+        {
+            printf("Tie !\n");
             break;
         }
+       
+        
     }
 }
 
 int main(){
     setBoard();
     drawBoard();
-    checkwinner();
+    checkWinner();
     return 0;
 }
